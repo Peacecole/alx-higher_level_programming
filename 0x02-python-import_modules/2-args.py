8320514d8934346ae1755f04977868f0aab6b953
Peacecole <onyangocollince@gmail.com>
2023-06-08 16:15:24 +0000

    Task 2 - a program that prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    count_argv = len(sys.argv) - 1
+    if count_argv == 0:
+        print("0 arguments.")
+    elif count_argv == 1:
+        print("1 arguments.")
+    else :
+        print("{} arguments:".format(count_argv))
+    for i in range(count_argv):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
