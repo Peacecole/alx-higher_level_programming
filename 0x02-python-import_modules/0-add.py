5ef3c471820f9f29f5fc8926c6c382affef5f9b0
Peacecole <onyangocollince@gmail.com>
2023-06-09 02:26:43 +0000

    Task 0 -  imports the function def add(a, b): from the file add_0.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """ imports the function def add(a, b): from the file add_0.p"""
     from add_0 import add
     a = 1
     b = 2
-    print("{} + {} - {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
