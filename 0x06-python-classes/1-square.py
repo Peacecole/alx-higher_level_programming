d89abe83d7ba7be775b38bc92bd13defe12b7508
Peacecole <onyangocollince@gmail.com>
2023-06-28 02:42:23 +0000

    Task 1 - a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Represents the class Square"""
+    def __init__(self, size):
+        """Intializes a new attribute to the class Square
+
+            Args:
+                size Iint): Integer value, the size of the new Square"""
+        self.__size = size
