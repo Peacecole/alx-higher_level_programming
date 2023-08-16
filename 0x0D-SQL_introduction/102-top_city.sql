ed5a667f6c90461f2dfdb8d44141ffe6e417c74b
Peacecole <onyangocollince@gmail.com>
2023-08-16 00:54:23 +0000

    Task 19 - Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Imports into a database a given table
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
