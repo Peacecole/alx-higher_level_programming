b3a1d5a00c4b46fc213531de3465e658b6ce25f8
Peacecole <onyangocollince@gmail.com>
2023-08-17 01:04:41 +0000

    Task 0 - a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- Lists all privileges of given users in a MySQL server
+-- first user user_0d_1
+-- second user user_0d_2
 SHOW GRANTS FOR `user_0d_1`@`localhost`;
 SHOW GRANTS FOR `user_0d_2`@`localhost`;
