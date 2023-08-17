e89c1ef83358c9de09958d7a67cf031d6638f204
Peacecole <onyangocollince@gmail.com>
2023-08-17 01:37:05 +0000

    Task 12 - a script that imports a database dump from hbtn_0d_tvshows to your MySQL server

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- Imports a given database to the MySQL server
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
