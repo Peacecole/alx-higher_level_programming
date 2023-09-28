495f3da0f9bb51f68d0d8215c2105e231b1afaa0
Peacecole <onyangocollince@gmail.com>
2023-09-28 14:24:19 +0000

    Task 4 - A Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in as an argument aand sends a GET request to a given URL then displays the body of the response
-curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
+curl -sX GET $1 -H "X-School-User-Id: 98" -L
