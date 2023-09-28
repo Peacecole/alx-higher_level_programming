4ecfa680cb47146ee19381f73f5727be9647e039
Peacecole <onyangocollince@gmail.com>
2023-09-28 15:03:03 +0000

    Task 9 - A Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a given messagein the body of the response

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that makes the server to respond with a given message in response body
-curl -o /dev/null -sw "You got me!" 54.236.26.82 :5000/catch_me
+curl -o /dev/null -sw "You got me!" 54.236.26.82:5000/catch_me
