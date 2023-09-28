91037723033d0f4941a3311419d873924e1172d0
Peacecole <onyangocollince@gmail.com>
2023-09-28 14:27:10 +0000

    Task 5 - A Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# takes in and sends a POST request to a given and passed URL, and displays the body of the response
+curl -sX POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD" -L
