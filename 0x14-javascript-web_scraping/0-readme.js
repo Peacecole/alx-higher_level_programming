f0f0e8d919ef40d0e18be54379322ee5ad5cd1ea
Peacecole <onyangocollince@gmail.com>
2023-10-25 02:44:15 +0000

    Task 0 - a script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,8 @@
 let fs = require('fs');
 let file = process.argv[2];
 function readPrint (file) {
-  fs.readFile(file, 'utf-8', function (err, data) {
+  fs.readFile(file, 'utf-8', 
+		  function (err, data) {
     if (err) {
       console.log(err);
     } else {
