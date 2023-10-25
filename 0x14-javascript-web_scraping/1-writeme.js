5a17df60ff8d026871d0fffd807413aeff8a8cbb
Peacecole <onyangocollince@gmail.com>
2023-10-25 02:56:08 +0000

    Task 1 - a script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,9 +4,9 @@ let file = process.argv[2];
 let content = process.argv[3];
 
 function writeMe (file, content) {
-  fs.writeFile(file, content, function (err) {
-    if (err) {
-      console.log(err);
+  fs.writeFile(file, content, function (error) {
+    if (error) {
+      console.log(error);
     }
   });
 }
